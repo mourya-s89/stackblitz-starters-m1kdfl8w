8fd8c5d673e15878dd2f3c4c23dc5d3d5f28fed8
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-11 11:59:56 +0530

    api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 
@@ -8,6 +9,11 @@ app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
 
 app.listen(port, () => {
